842cdc2ab5fb7347b6962a8e39bb116c90ee4ec0
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2020-12-12 10:32:16 +0400

    slider

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainContentComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    // window.addEventListener('wheel', function(event){
+    //   if (event.deltaY < 0){
+    //     console.log('scrolling up');
+    //   }
+    //   else if (event.deltaY > 0)
+    //   {
+    //     console.log('scrolling down');
+    //   }
+    // });
+
+    (function() {
+      window.onresize = displayWindowSize;
+      window.onload = displayWindowSize;
+
+      function displayWindowSize() {
+        let myHeight = window.innerHeight;
+        document.querySelector(".carousel__viewport").setAttribute("style",`height:${myHeight + 17}px`);
+      };
+    })();
+  }
 }
